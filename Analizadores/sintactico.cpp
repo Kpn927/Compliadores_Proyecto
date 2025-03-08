275fc21cf205022607131af80b50c280818241bc
Sebangel <angeljavicueche@gmail.com>
2025-03-08 16:05:56 -0400

    feat: se añade phraseAnalizer y verifyAsignation, refactor: recursiveTree lee parentesis

--- Analizadores/sintactico.cpp
+++ Analizadores/sintactico.cpp
@@ -46,7 +46,7 @@ void Syntax::getSyntaxBegin(vector<string> &datos, vector<string> &symbols, int 
                     return;
                 } else {
                     cout << "Begin-End terminado" << endl;
-                    return;
+                    initialNumber++;
                 }
             }
         } 
@@ -65,20 +65,16 @@ void Syntax::getSyntaxBegin(vector<string> &datos, vector<string> &symbols, int 
             int result = forAnalizer(datos, initialNumber);
             if (result == -1) return; 
             initialNumber = result;
-        }
-        
+        }else if (datos[initialNumber] == ";") initialNumber++;
         else {
-            int finalNumber = getFinalNumber(initialNumber, datos);
-            
             int i = 0;
             while (i < variables.size()) {
-                if (datos[initialNumber] == variables[i]) {
+                if (datos[initialNumber] == variables[i].name) {
                     getSyntaxAsignation(datos, symbols, initialNumber);
-                    break;
+                    i = variables.size();
                 }
                 i++;
             }
-            initialNumber = finalNumber + 2; 
         }
     }
 }
@@ -100,18 +96,20 @@ void Syntax::getSyntaxVar(vector<string> &datos, vector<string> &symbols, int &i
 
     Node<string> *a = arbolvar.getRaiz()->getLeft();
     Node<string> *b = arbolvar.getRaiz()->getRight();
+    Variables newVariable;
 
     if (a->getValue() != "")
     {
         for (int i = 0; i < variables.size(); i++)
         {
-            if (variables[i] == a->getValue())
+            if (variables[i].name == a->getValue())
             {
                 cout << "ERROR: VARIABLE DOBLEMENTE DECLARADA." << endl;
                 exit(1);
             }                    
         }
-        variables.push_back(a->getValue());
+        newVariable.name = a->getValue();
+        variables.push_back(newVariable);
     }
     
     try
@@ -175,7 +173,7 @@ void Syntax::getSyntaxVar(vector<string> &datos, vector<string> &symbols, int &i
             {
                 for (int i = 0; i < variables.size(); i++)
                 {
-                    if ((variables[i] == a->getLeft()->getValue()) || (variables[i] == a->getRight()->getValue()))
+                    if ((variables[i].name == a->getLeft()->getValue()) || (variables[i].name == a->getRight()->getValue()))
                     {
                         flag = false;
                         cout << "ERROR: VARIABLE YA DECLARADA." << endl;
@@ -186,12 +184,14 @@ void Syntax::getSyntaxVar(vector<string> &datos, vector<string> &symbols, int &i
 
             if (flag)  
             {
-                variables.push_back(a->getLeft()->getValue());
+                newVariable.name = a->getLeft()->getValue();
+                variables.push_back(newVariable);
                 a = a->getRight();
 
                 if (a->getValue() != "")
                 {   
-                    variables.push_back(a->getValue());
+                    newVariable.name = a->getValue();
+                    variables.push_back(newVariable);
                 }
                 
             } else {
@@ -209,7 +209,62 @@ void Syntax::getSyntaxVar(vector<string> &datos, vector<string> &symbols, int &i
         initial = final + 2;
 }
 
-void Syntax::getSyntaxAsignation(vector<string> &datos, vector<string> &symbols, int initial) {
+string Syntax::phraseAnalizer(Node<string> *nodo){
+    if (nodo->getRight()==NULL){
+        return nodo->getValue();
+    }
+
+    if (nodo->getCenter()->getCenter() != NULL){
+        string value = phraseAnalizer(nodo->getCenter());
+        return value;
+    }else {
+        if (nodo->getRight()->getRight() != NULL){
+            string value = phraseAnalizer(nodo->getRight());
+            nodo->getRight()->setValue(value);
+        }
+        if (nodo->getLeft()->getLeft() != NULL){
+            string value = phraseAnalizer(nodo->getLeft());
+            nodo->getLeft()->setValue(value);
+        }
+    }
+    int i = 0;
+    int *left, *right , result;
+    if(Check::isNumber(nodo->getLeft()->getValue())){
+         left = new int;
+        *left =std::stoi(nodo->getLeft()->getValue());
+    } 
+    if(Check::isNumber(nodo->getRight()->getValue())){
+        right = new int;
+        *right =std::stoi(nodo->getRight()->getValue());
+   }  
+    while(i<variables.size()){
+
+        if(variables[i].name == nodo->getLeft()->getValue() && !variables[i].value.empty()){
+            if (Check::isNumber(variables[i].value)){
+                left = new int;
+                *left =(int) std::stoi(variables[i].value);
+            }
+        }
+        if(variables[i].name == nodo->getRight()->getValue() && !variables[i].value.empty()){
+            if (Check::isNumber(variables[i].value)){
+                right = new int;
+                *right =(int) std::stoi(variables[i].value);
+            }
+        }
+        i++;
+    }
+    if(left!=NULL && right!=NULL){
+        if (nodo->getCenter()->getValue() == "+") result = *left + *right;
+        if (nodo->getCenter()->getValue() == "-") result = *left - *right;
+        if (nodo->getCenter()->getValue() == "*") result = *left * *right;
+        if (nodo->getCenter()->getValue() == "/") result = *left / *right;
+        return (string)std::to_string(result);
+    } else{
+        return nodo->getLeft()->getValue() + " " + nodo->getCenter()->getValue() + " " + nodo->getRight()->getValue();
+    }
+}
+
+void Syntax::getSyntaxAsignation(vector<string> &datos, vector<string> &symbols, int &initial) {
     int final = getFinalNumber(initial, datos);
     Tree<string> arbolvar;
     
@@ -228,7 +283,7 @@ void Syntax::getSyntaxAsignation(vector<string> &datos, vector<string> &symbols,
 
         for (int i = 0; i < variables.size(); i++)
         {
-            if (b->getValue() == variables[i])
+            if (b->getValue() == variables[i].name)
             {
                 flag = false;
             } 
@@ -245,35 +300,20 @@ void Syntax::getSyntaxAsignation(vector<string> &datos, vector<string> &symbols,
         std::cerr << e.what() << '\n';
     }
     
-
-    try
-    {
-        bool left = false;
-        bool right = false;
-        while (a->getRight() != NULL)
-        {    
-            left = isNumber(a->getLeft()->getValue());
-            right = isNumber(a->getRight()->getValue());
-            if (!right && a->getRight()->getValue() == "") right=true;
-            if(!(left && right)){
-                for (int i = 0; i < variables.size(); i++)
-                {
-                    if (variables[i] == a->getLeft()->getValue()) left = true;
-                    if (variables[i] == a->getRight()->getValue()) right = true;     
-                }
-            }
-            if (!(left && right))
-            {
-                cout<<"ERROR: NO PUSISTE BIEN LA ASIGNACION DE VARIABLE"<<endl;
-                return;
-            }
-            a = a->getRight();
-        }
+    if (!Check::verifyAsignation(arbolvar.getRaiz())){
+        cout<<"ERROR: NO PUSISTE BIEN LA ASIGNACION DE VARIABLE"<<endl;
+        return;
     }
-    catch(const std::exception& e)
-    {
-        std::cerr << e.what() << '\n';
+        
+    initial = final+2;
+    string hola = (string) phraseAnalizer(arbolvar.getRaiz()->getRight());
+    int i = 0;
+    while(i<variables.size()){
+        if(arbolvar.getRaiz()->getLeft()->getValue() == variables[i].name) variables[i].value = hola;
+        i++;
     }
+    cout<<"[resultado: "<<hola<<"]"<<endl;
+    return;
 }
 
 // Faltaria la logica del // para ver como terminariamos la linea
@@ -426,26 +466,43 @@ void Syntax::recursiveTree(Tree<string>&padre, vector<string> &datos, int initia
 void Syntax::recursiveTree(Node<string>* actual, Tree<string>&padre, vector<string> &symbols, vector<string> &datos, int initialNumber, int finalNumber){
     // detectar separador
     int symbol;
-    for(int i = 0; i< symbols.size(); i++){
-        for(int j = initialNumber; j<=finalNumber;j++){
-            if (symbols[i]==datos[j]){
-                padre.insertarOrdenado(datos[j], 1, actual);
-                symbol=j;
-                j= finalNumber+1;
-                i = symbols.size() +1;
+    if (actual==NULL) return;
+    if (datos[initialNumber] == "("){
+        padre.insertarOrdenado(datos[initialNumber],0,actual);
+        initialNumber++;
+        finalNumber=initialNumber;
+        while(datos[finalNumber]!=")"){
+            finalNumber++;
+        }
+        if(datos[finalNumber] == ")") padre.insertarOrdenado(datos[finalNumber],2,actual);
+        if(finalNumber-initialNumber== 1) cout<<"NADA ENTRE LOS PARENTESIS"<<endl;
+        else if(finalNumber-initialNumber== 2) padre.insertarOrdenado(datos[finalNumber],1,actual);
+        else if(finalNumber-initialNumber> 2) {
+            padre.insertarOrdenado("", 1, actual);
+            recursiveTree(actual->getCenter(), padre, symbols, datos, initialNumber, finalNumber-1);
+        }
+    }else{
+        for(int i = 0; i< symbols.size(); i++){
+            for(int j = initialNumber; j<=finalNumber;j++){
+                if (symbols[i]==datos[j]){
+                    padre.insertarOrdenado(datos[j], 1, actual);
+                    symbol=j;
+                    j= finalNumber+1;
+                    i = symbols.size() +1;
+                }
             }
         }
-    }
-    if (symbol==initialNumber+1) padre.insertarOrdenado(datos[symbol-1], 0, actual);
-    else {
-        padre.insertarOrdenado("", 0, actual);
-        recursiveTree(actual->getLeft(), padre, symbols, datos, initialNumber, symbol-1);
-    }
-
-    if (symbol==finalNumber-1) padre.insertarOrdenado(datos[symbol+1], 2, actual);
-    else {
-        padre.insertarOrdenado("", 2, actual);
-        recursiveTree(actual->getRight(), padre, symbols, datos, symbol+1, finalNumber);
+        if (symbol==initialNumber+1) padre.insertarOrdenado(datos[symbol-1], 0, actual);
+        else {
+            padre.insertarOrdenado("", 0, actual);
+            recursiveTree(actual->getLeft(), padre, symbols, datos, initialNumber, symbol-1);
+        }
+    
+        if (symbol==finalNumber-1) padre.insertarOrdenado(datos[symbol+1], 2, actual);
+        else {
+            padre.insertarOrdenado("", 2, actual);
+            recursiveTree(actual->getRight(), padre, symbols, datos, symbol+1, finalNumber);
+        }
     }
     return;
 }
